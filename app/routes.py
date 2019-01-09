8d522d7768be61f853c4239d8ff0041faed91e53
Isaac Friedman <isaac@isaac-friedman.com>
2019-01-09 16:21:37 -0500

    routes for all pages from old static site

---
+++ app/routes.py
@@ -0,0 +1,41 @@
+from flask import Flask, flash, redirect, render_template, request, url_for
+from werkzeug.urls import url_parse
+from app import app
+#from app import db
+from app.models import Deceased
+from datetime import datetime
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/akahn')
+def akahn():
+    return render_template("akahn.html")
+
+@app.route('/mkahn')
+def mkahn():
+    return render_template("mkahn.html")
+
+@app.route('/friedman')
+def friedman():
+    return render_template("friedman.html")
+
+@app.route('/yamnik')
+def akahn():
+    return render_template("yamnik.html")
+
+@app.route('/memorial', methods=["GET", "POST"])
+def memorial():
+    return "Db in progress. Memorials will be up soon."
+'''
+    plaques = []
+    #records = Deceased.query.all()
+    records = db.session.query(Deceased).order_by(Deceased.full_name)
+    for record in records:
+        send_date = f'{record.secular_death_date:%A} {record.secular_death_date:%B} {record.secular_death_date.day}, {record.secular_death_date.year}'
+        plaques.append({"first_name" : record.first_name, "last_name" : record.last_name, "secular_death_date" : send_date})
+        print(plaques)
+    return render_template('memorial.html', plaques=plaques)
+''''
