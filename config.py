5c526e56533e90f4e5a5c544c6ab7c0f1b757993
Isaac Friedman <isaac@isaac-friedman.com>
2019-01-13 06:24:16 -0500

    configured db

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'there will be a 128-bit random string here at some point'
     TESTING = False 
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'app.db')
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- mcs-flask.py
+++ mcs-flask.py
@@ -1,8 +1,6 @@
-from app import app
-#from app.models import User, Post
+from app import app, db
+from app.models import Deceased
 
-'''
 @app.shell_context_processor
 def make_shell_context():
     return {'db':db, 'Deceased': Deceased} 
-'''
