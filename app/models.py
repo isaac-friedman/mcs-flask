c20c328f93b4fa45dfc3d7d3b9b627eb9ba6bfdc
Isaac Friedman <isaac@isaac-friedman.com>
2019-01-31 13:02:24 -0500

    Bringing models up to date somehow missed in last commit

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Deceased(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    full_name = db.Column(db.String(64), index=True)
+    last_name = db.Column(db.String(64))
+    first_name = db.Column(db.String(64))
+    secular_death_date = db.Column(db.DateTime, index=True)
+    tablet_column = db.Column(db.Float, index=True)
+    bio = db.Column(db.Text)
+    order = db.Column(db.Integer, index=True)
+
+    def __repr__(self):
+        return '<Deceased {}>'.format(self.full_name)
