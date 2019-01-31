fa8cbffc0aeef2274e34b0dec081d9eb689ae1c3
Isaac Friedman <isaac@isaac-friedman.com>
2019-01-31 16:55:49 -0500

    final decision on type for jewish death date

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Deceased(db.Model):
     full_name = db.Column(db.String(64), index=True)
     last_name = db.Column(db.String(64))
     first_name = db.Column(db.String(64))
-    jewish_death_date = db.Column(db.String(64))
+    jewish_death_date = db.Column(db.String(64), index=True)
     secular_death_date = db.Column(db.DateTime, index=True)
     tablet_column = db.Column(db.Float, index=True)
     bio = db.Column(db.Text)
